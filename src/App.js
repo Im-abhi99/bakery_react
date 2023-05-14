465a722d3e4022e4e8c01a4904a4d8e2d45e8bec
Im-abhi99 <imabhishekrk@gmail.com>
2023-05-14 03:49:42 -0700

    chages made in index and app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
   <>
   <Routes>
-        <Route path="/home" index element={<Home />} />
+        <Route path="/" index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Notfound />} />
    </Routes>
